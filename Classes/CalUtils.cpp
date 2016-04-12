f4cd8c8be997d5a8b5100ba1b8e862038cc92603
mixi57 <mixi57@126.com>
2016-04-12 23:35:13 +0800

    之前的代码是某个夜晚被舍友锁在门外，在全家通宵出来的作品，所以，说明，睡眠不足严重影响成品质量。尝试重构了下

---
+++ Classes/CalUtils.cpp
@@ -0,0 +1,9 @@
+//
+//  CalUtils.cpp
+//  9Game
+//
+//  Created by mixi on 16/4/11.
+//
+//
+
+#include "CalUtils.hpp"
---
+++ Classes/CalUtils.hpp
@@ -0,0 +1,14 @@
+//
+//  CalUtils.hpp
+//  9Game
+//
+//  Created by mixi on 16/4/11.
+//
+//
+
+#ifndef CalUtils_hpp
+#define CalUtils_hpp
+
+#include <stdio.h>
+
+#endif /* CalUtils_hpp */
